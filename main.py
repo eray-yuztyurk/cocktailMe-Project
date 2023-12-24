d41c7eaf81bbf26a22ce5144b46b07d6907add31
Eray Yuztyurk <77536463+eray-yuztyurk@users.noreply.github.com>
2023-12-24 16:04:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ style = """
 st.markdown(style, unsafe_allow_html=True)
 
 logo = st.container(border=True)
-logo.image("img/cocktailMe_logo.png", use_column_width=True)
+logo.image("img/cocktailMe_logo.png", use_column_width="auto")
 welcome, cocktail_finder, measurement = st.tabs(["Welcome", "Cocktail Finder", "Measurements"])
 
 ########################################################################################################################
